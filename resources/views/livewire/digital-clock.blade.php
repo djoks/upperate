1d1b3d9502f5809a896093ecc3a28499a932b062
djoks <contact.djoks@gmail.com>
2025-02-25 00:07:33 +0000

    refactor: improve header responsiveness.

--- resources/views/livewire/digital-clock.blade.php
+++ resources/views/livewire/digital-clock.blade.php
@@ -1,22 +1,20 @@
-<div wire:ignore x-data="DigitalClock()" class="flex">
-    <x-card class="w-48 self-center rounded-box bg-transparent items-center justify-center">
-        <div class="flex justify-center items-center gap-2">
-            <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
-                id="hours" x-text="localHours">
-                <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Hours</div>
-            </div>
-            :
-            <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
-                id="hours" x-text="localMinutes">
-                <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Minutes</div>
-            </div>
-            :
-            <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
-                id="hours" x-text="localSeconds">
-                <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Seconds</div>
-            </div>
+<div wire:ignore x-data="DigitalClock()" class="flex justify-center my-5 md:py-0 md:justify-normal">
+    <div class="flex justify-center items-center gap-2">
+        <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
+            id="hours" x-text="localHours">
+            <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Hours</div>
         </div>
-    </x-card>
+        :
+        <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
+            id="hours" x-text="localMinutes">
+            <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Minutes</div>
+        </div>
+        :
+        <div class="relative overflow-hidden bg-black/50 rounded-lg p-4 w-[50px] text-base font-bold hover:scale-105 transition-transform duration-200 ease-out"
+            id="hours" x-text="localSeconds">
+            <div class="absolute bottom-[-25px] left-0 w-full text-base text-[0.8rem] uppercase">Seconds</div>
+        </div>
+    </div>
 </div>
 
 @script
--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,24 +1,22 @@
 <div>
-    <x-header class="flex-col justify-center items-center w-full md:flex-row">
-        <x-slot name="title" class="text-xl font-regular">
-            <div class="flex items-center space-x-2">
-                <img src="{{ asset('assets/logos/app.png') }}" alt="Upperate Logo" class="h-10 w-10">
-                <div class="flex flex-col">
-                    <span>Upperate Crypto Exchange</span>
-                    <span class="text-xs font-light text-green-600 dark:text-amber-500">Currently viewing
-                        ({{ count($exchanges) }}) exchanges</span>
-                </div>
+    <!-- HEADER -->
+    <div class="flex flex-col md:flex-row justify-center md:justify-between items-center w-full md:my-5">
+        <div class="flex w-auto items-center justify-center space-x-2 text-xl font-regular">
+            <img src="{{ asset('assets/logos/app.png') }}" alt="Upperate Logo" class="h-10 w-10">
+            <div class="flex flex-col">
+                <span>Upperate Crypto Exchange</span>
+                <span class="text-xs font-light text-green-600 dark:text-amber-500">Currently viewing
+                    ({{ count($exchanges) }}) exchanges</span>
             </div>
-        </x-slot>
-        <x-slot name="actions">
-            <livewire:digital-clock />
-        </x-slot>
-    </x-header>
+        </div>
+
+        <livewire:digital-clock />
+    </div>
 
     <!-- TABLE  -->
     <x-card>
-        <x-table :headers="$headers" :rows="$exchanges" wire:model="expanded" expandable expandable-key="exchange"
-            no-headers show-empty-text no-hover :key="uniqid()">
+        <x-table :headers="$headers" :rows="$exchanges" wire:model="expanded" expandable expandable-key="exchange" no-headers
+            show-empty-text no-hover :key="uniqid()">
             @scope('cell_exchange', $exchanges)
                 <div class="flex items-center space-x-2 capitalize">
                     <livewire:exchange-logo :exchange="$exchanges['exchange']" />
@@ -46,6 +44,7 @@
         </x-table>
     </x-card>
 
+    <!-- FOOTER -->
     <div>
         <div class="text-center text-xs py-5">This is project was built by Philip.</div>
     </div>
